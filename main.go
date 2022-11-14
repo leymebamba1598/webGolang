12e53e3503a92de075da8da74915e3c7d7a3908c
Richar Didier Añazco Llaja <ridear1598@gmail.com>
2022-11-14 18:24:20 -0500

    REdireccionar urls (redirect)

--- main.go
+++ main.go
@@ -9,7 +9,13 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hola mundo")
+		w.Header().Add("Nombre", "Valor del header")
+		w.Header().Add("Nombre2", "Valor del header dos")
+		//fmt.Fprintf(w, "Hola mundo")
+
+		//Redireccionar url
+		http.Redirect(w, r, "/dos", http.StatusMovedPermanently)
+
 	})
 
 	http.HandleFunc("/dos", func(w http.ResponseWriter, r *http.Request) {
