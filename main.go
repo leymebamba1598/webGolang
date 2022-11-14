07eff79e9c81f1668789aaa478d4ba3cec148921
Richar Didier Añazco Llaja <ridear1598@gmail.com>
2022-11-14 18:03:17 -0500

    uso de HandlFunc

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hola mundo")
+	})
+
+	http.HandleFunc("/dos", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Numero dos")
+	})
 	//Iniciar un servidor en go
 	err := http.ListenAndServe("localhost:3000", nil)
 	if err != nil {
