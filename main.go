259bd5cc16b5ba64aef59eba723d96d545aa5746
Richar Didier Añazco Llaja <ridear1598@gmail.com>
2022-11-15 05:03:35 -0500

    Obtener argumentos d ela url

--- main.go
+++ main.go
@@ -8,20 +8,18 @@ import (
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("El metodo es ", r.Method)
-		switch r.Method {
-		case "GET":
-			fmt.Fprintf(w, "Hola mundo el metodo es GET")
-		case "POST":
-			fmt.Fprintf(w, "Hola el metodo es POST")
-		case "PUT":
-			fmt.Fprintf(w, "Hola el metodo es PUT")
-		case "DELETE":
-			fmt.Fprintf(w, "Hola el metodo es DELETE")
-		default:
-			http.Error(w, "Metodo no valido", http.StatusBadRequest)
+	http.HandleFunc("/url", func(w http.ResponseWriter, r *http.Request) {
+		//fmt.Println(r.URL.RawQuery)//Ver parametros enviados por url
+
+		fmt.Println(r.URL.Query()) //Retorna un mapa
+		//Obtenemos los datos del mapa
+
+		name := r.URL.Query().Get("name")
+		if len(name) != 0 {
+			fmt.Println("Nombre", name)
 		}
+		fmt.Fprintf(w, "Hola mundo")
+
 	})
 	//Iniciar un servidor en go
 	err := http.ListenAndServe("localhost:3000", nil)
