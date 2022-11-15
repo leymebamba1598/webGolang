3fa25d1a56bd8e9339a402ffef0c9d191fd5f95d
Richar Didier Añazco Llaja <ridear1598@gmail.com>
2022-11-15 05:57:38 -0500

    Leer encabezados(headers)

--- main.go
+++ main.go
@@ -9,23 +9,21 @@ import (
 func main() {
 
 	http.HandleFunc("/params", func(w http.ResponseWriter, r *http.Request) {
-		//fmt.Println(r.URL.RawQuery) Ver Query completo
-		//fmt.Println(r.URL.Query())  Ver mapa
+		fmt.Println(r.Header)
 
-		fmt.Println(r.URL)
-		values := r.URL.Query()
+		//Leer headers
+		accesToken := r.Header.Get("acces_token")
+		if len(accesToken) != 0 {
+			fmt.Println(accesToken)
+		}
+		//agregar encabezados
+		r.Header.Set("nombre", "didier")
+		fmt.Println(r.Header)
 
-		values.Del("otro") //elimina un parametro
-
-		values.Add("name", "Didier")
-		values.Add("curos", "Goweb")
-		values.Add("edad", "24")
-		r.URL.RawQuery = values.Encode()
-
-		fmt.Println(r.URL)
 		fmt.Fprintf(w, "Hola mundo")
 
 	})
+
 	//Iniciar un servidor en go
 	err := http.ListenAndServe("localhost:3000", nil)
 	if err != nil {
