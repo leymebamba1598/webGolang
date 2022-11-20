396795048fac13a300a5be25cb3c51046031cbba
Richar Didier Añazco Llaja <ridear1598@gmail.com>
2022-11-20 02:44:54 -0500

    Nuevo template

--- main.go
+++ main.go
@@ -4,30 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
 
 //Server mux -- Listado de rutas las cuales tienen asociadas acciones
 //Handlers -- acciones asociadas a las rutas (responder al cliente (cuerpo, encabezado, status))
 
-type customHandelers func(w http.ResponseWriter, r *http.Request)
-
-type MuxFacilito struct {
-	rutasFaciitas map[string]customHandelers //Handlers
-}
-
-func (this *MuxFacilito) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fn := this.rutasFaciitas[r.URL.Path]
-	fn(w, r)
-}
-func (this *MuxFacilito) AddMux(ruta string, fn customHandelers) {
-	this.rutasFaciitas[ruta] = fn
-}
 func main() {
-	newMapa := make(map[string]customHandelers)
-	mux := &MuxFacilito{rutasFaciitas: newMapa}
-
-	mux.AddMux("/Hola", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hola desde funcion anonima")
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		template, err := template.New("Hola").Parse("Hola mundo")
+		if err != nil {
+			panic(err)
+		}
+		template.Execute(writer, nil)
 	})
-	log.Fatal(http.ListenAndServe("localhost:3000", mux))
+	fmt.Println("Servidor corriendo en el puerto: 3000 ")
+	log.Fatal(http.ListenAndServe("localhost:3000", nil))
 }
