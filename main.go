9d4858235d629bd54eee40dfdbf8ca3b41562fcd
Richar Didier Añazco Llaja <ridear1598@gmail.com>
2022-11-20 03:17:43 -0500

    paginas dinamicas

--- main.go
+++ main.go
@@ -9,14 +9,18 @@ import (
 
 //Server mux -- Listado de rutas las cuales tienen asociadas acciones
 //Handlers -- acciones asociadas a las rutas (responder al cliente (cuerpo, encabezado, status))
+type Usuaurio struct {
+	Username string
+}
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		template, err := template.New("Hola").Parse("Hola mundo")
+		template, err := template.ParseFiles("templates/index.html")
 		if err != nil {
 			panic(err)
 		}
-		template.Execute(writer, nil)
+		user := Usuaurio{"Didier"}
+		template.Execute(writer, user)
 	})
 	fmt.Println("Servidor corriendo en el puerto: 3000 ")
 	log.Fatal(http.ListenAndServe("localhost:3000", nil))
