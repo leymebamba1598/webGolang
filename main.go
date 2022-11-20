b3cd0c92f03158690f7df39f28d41a89a97bf658
Richar Didier Añazco Llaja <ridear1598@gmail.com>
2022-11-20 00:50:11 -0500

    (Modulo 2: Servidor) New server mux

--- main.go
+++ main.go
@@ -9,19 +9,24 @@ import (
 //Server mux -- Listado de rutas las cuales tienen asociadas acciones
 //Handlers -- acciones asociadas a las rutas (responder al cliente (cuerpo, encabezado, status))
 
+func Hola(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hola mundo desde mux DefaultServerMux")
+}
 func Hola2(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hola mundo desde funcion")
+	fmt.Fprintf(w, "Hola mundo desde mux propio")
 }
 func main() {
+	http.HandleFunc("/", Hola) //DefaultServerMux
 
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, "Hola mundo")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Hola2)
+	mux.HandleFunc("/dos", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, "Hola mundo desde mux propio dos")
 	})
-	http.HandleFunc("/hola2", Hola2) //DefaultServerMux
 
 	server := &http.Server{
 		Addr:    "localhost:3000",
-		Handler: nil, //Si es nill utilizamos el DefaultServerMux
+		Handler: mux, //Si es nill utilizamos el DefaultServerMux
 	}
 
 	err := server.ListenAndServe()
